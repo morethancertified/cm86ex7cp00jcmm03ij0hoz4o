2ebdce91c05ee3fc04d7f975a5ae55646329556a
Terraform <terraform@example.com>
2025-03-21 08:31:03 -0500

    Add public files

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "github_repository" "infra-dev" {
+  name               = "infra-dev"
+  description        = "Infrastructure Dev Repository"
+  auto_init          =  true
+  visibility         = "private"
+  gitignore_template = "Terraform"
+}
+
+resource "github_branch" "infra-dev-staging" {
+  repository = github_repository.infra-dev.name
+  branch     = "staging"
+}
+
+resource "github_repository" "infra-prod" {
+  name               = "infra-prod"
+  description        = "Infrastructure Prod Repository"
+  auto_init          =  true
+  visibility         = "public"
+  gitignore_template = "Terraform"
+}
+
+resource "github_branch" "infra-prod-staging" {
+  repository = github_repository.infra-prod.name
+  branch     = "staging"
+}
